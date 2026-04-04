354949059a9df034ee314aed07b86eb5731cad38
Aman Karn <amankarn2007@gmail.com>
2026-04-05 00:15:44 +0530

    frontend build

---
+++ binance-frontend/app/markets/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Page() {
+    return (
+        <div className="flex flex-row flex-1">
+            <div className="flex flex-col justify-center items-center flex-1 pt-25">
+                Markets Page
+            </div>
+        </div>
+    )
+}
