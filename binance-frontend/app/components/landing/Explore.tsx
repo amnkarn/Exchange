695c945f83c4c16126e88ba4a2d2cfeea4adb29e
Aman Karn <amankarn2007@gmail.com>
2026-04-08 23:03:39 +0530

    sec3

--- binance-frontend/app/components/landing/Explore.tsx
+++ binance-frontend/app/components/landing/Explore.tsx
@@ -2,11 +2,12 @@ import Sec1 from "./Components/ExploreSection1"
 import SectionsInfo from "./Components/SectionInfo"
 import Sec2 from "./Components/Section2"
 import Sec2Box from "./Components/Section2Box"
+import Sec3 from "./Components/Section3"
 
 
 export default function Explore() {
     return (
-        <div className="w-full flex flex-col items-center pt-30 bg-[#121218] font-interFam  pb-80">
+        <div className="w-full flex flex-col items-center pt-30 bg-[#121218] font-interFam pb-20">
             <div className="flex flex-col w-[60%] items-center">
                 <h2 className="text-[45px] font-bold">Built for traders who&thinsp;
                     <span className="text-red-500">demand more</span>.
@@ -16,27 +17,9 @@ export default function Explore() {
 
             <Sec1 />
             <Sec2 />
-
             <Sec3 />
-        </div>
-    )
-}
 
-function Sec3() {
-    return (
-        <div className="w-full">
-
-
-            <SectionsInfo 
-                headingLabel=""
-                heading=""
-                para=""
-                point1=""
-                point2=""
-                point3=""
-                tickBGColor=""
-                tickColor=""
-            />
+            
         </div>
     )
 }
