21899a30164eb224c29a8cee80901f5893d0e8b1
Aman Kumar Karn <amankarn2007@gmail.com>
2026-06-28 22:54:48 +0530

    added User, UserManager class

--- optimized/ws/src/User.ts
+++ optimized/ws/src/User.ts
@@ -1,5 +1,47 @@
+import type WebSocket from "ws";
+import { SubscriptionManager } from "./SubscriptionManager.js";
+import type { OutgoingMessage } from "./types/out.js";
+import { SUBSCRIBE, UNSUBSCRIBE, type IncommingMessage } from "./types/in.js";
 
 
 export class User {
-    
+    private id: string;
+    private ws: WebSocket;
+    private subscriptions: string[] = [];
+
+    constructor(id: string, ws: WebSocket) {
+        this.id = id;
+        this.ws = ws;
+        this.addListeners();
+    }
+
+    emit(message: OutgoingMessage) {
+        this.ws.send(JSON.stringify(message));
+    }
+
+    public subscribe(subscription: string) {
+        this.subscriptions.push(subscription);
+    }
+
+    public unsubscribe(subscription: string) {
+        this.subscriptions = this.subscriptions.filter(s => s !== subscription);
+    }
+
+    public addListeners() {
+        this.ws.on("message", (message: string) => {
+            const parsedMessage: IncommingMessage = JSON.parse(message);
+
+            if(parsedMessage.method === SUBSCRIBE) {
+                parsedMessage.params.forEach((s) => {
+                    SubscriptionManager.getInstanse().subscribe(this.id, s);
+                })
+            }
+
+            if(parsedMessage.method === UNSUBSCRIBE) {
+                parsedMessage.params.forEach((s) => {
+                    SubscriptionManager.getInstanse().unsubscribe(this.id, s);
+                })
+            }
+        })
+    }
 }
--- optimized/ws/src/UserManager.ts
+++ optimized/ws/src/UserManager.ts
@@ -1,9 +1,11 @@
+import type WebSocket from "ws";
 import { User } from "./User.js";
+import { SubscriptionManager } from "./SubscriptionManager.js";
 
 
 export class UserManager {
     private static instanse: UserManager;
-    private users = new Map<string, User>();
+    private users: Map<string, User> = new Map();
 
     constructor() {
 
@@ -17,16 +19,27 @@ export class UserManager {
         return this.instanse;
     }
 
-    public static addUser(ws: any) {
+    public addUser(ws: any) {
         const id = this.getRandomId();
         const user = new User(id, ws);
+        this.users.set(id, user);
+        this.registerOnClose(ws, id);
+        
+        return user;
+    }
+
+    public registerOnClose(ws: WebSocket, id: string) {
+        ws.on("close", () => {
+            this.users.delete(id);
+            SubscriptionManager.getInstanse().userLeft(id);
+        })
     }
 
     public getUser(id: string) {
-        
+        return this.users.get(id);
     }
 
-    private static getRandomId() {
+    private getRandomId() {
         return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
     }
 }
